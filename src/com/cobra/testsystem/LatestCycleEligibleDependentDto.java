77a1715b80f048403020202358f74ec565b80e15
habandeh <hashem_abandeh@yahoo.com>
2023-05-31 10:05:22 +0300

    TEST GIT

---
+++ src/com/cobra/testsystem/LatestCycleEligibleDependentDto.java
@@ -0,0 +1,20 @@
+package com.cobra.testsystem;
+
+public class LatestCycleEligibleDependentDto {
+
+	private Long dependentNin;
+	
+	public LatestCycleEligibleDependentDto(Long dependentNin) {
+
+		this.dependentNin = dependentNin;
+	}
+
+	public Long getDependentNin() {
+		return dependentNin;
+	}
+
+	public void setDependentNin(Long dependentNin) {
+		this.dependentNin = dependentNin;
+	}
+	
+}
---
+++ src/com/cobra/testsystem/MyMain.java
@@ -0,0 +1,23 @@
+package com.cobra.testsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+
+        List<LatestCycleEligibleDependentDto> programLatestCycleEligibleDependents = new ArrayList();//.getLatestCycleAllEligibleDependents(applicationId, programId);
+
+        programLatestCycleEligibleDependents.add(new LatestCycleEligibleDependentDto(123l));
+        List<Long> programLatestCycleEligibleDependentsNin = programLatestCycleEligibleDependents.stream()
+                .map(LatestCycleEligibleDependentDto::getDependentNin)
+                .collect(Collectors.toList());
+        
+        System.out.println(programLatestCycleEligibleDependentsNin.size());
+        System.out.println(programLatestCycleEligibleDependentsNin.get(0));
+        System.out.println("Done");
+	}
+
+}
